6d6361c7b9a0135ec374c30aeada883f34cfa3c3
Annyz16 <ngohuyld.167@gmail.com>
2016-09-26 08:14:31 +0700

    bai test

---
+++ ConsoleApplication1/ConsoleApplication1/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Node<N>
+    {
+        N Node;
+        private N data;
+
+        public N Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        Node<N> next;
+
+        internal Node<N> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+    }
+}
