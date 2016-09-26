9f8158c24067e9776c5af274805c48776156abf7
Annyz16 <ngohuyld.167@gmail.com>
2016-09-26 09:17:59 +0700

    43rtretreter

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List<N>
+    {
+        Node<N> head;
+
+        internal Node<N> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+
+        Node<N> tail;
+
+        internal Node<N> Tail
+        {
+            get { return tail; }
+            set { tail = value; }
+        }
+
+       
+    }
+}
